2afb239c75142188a055ad95b4c94b8db0bf49f1
AvgustChen <avchen@yandex.ru>
2022-07-15 14:18:51 +0300

    add HomeWork3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void CubsNumbers(int number)
+{
+    if (number < 0)
+    {
+        Console.WriteLine("Вы ввели отрицательное число, поэтому мы сами его преобразовали в положительное ;)");
+        number = number * -1;
+    }
+    if (number == 0)
+    {
+        Console.WriteLine("Вы ввели 0, чисел нет!");
+    }
+    int count = 1;
+    while (count <= number)
+    {
+        int result = count * count * count;
+        Console.Write(result + " ");
+        count++;
+    }
+}
+Console.WriteLine("Здравствуйте!");
+Console.Write("Введите число N и программа выдаст таблицу кубов чисел от 1 до N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CubsNumbers(number);
+Console.WriteLine();
+Console.Write("Хорошего вам дня! ;)");
