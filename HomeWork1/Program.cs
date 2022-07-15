9d00b9811ea8f28c148aa66dece01d6932b11bd3
AvgustChen <avchen@yandex.ru>
2022-07-15 15:45:43 +0300

    Добавил второе решение задачи если число будет любым

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-void Numbers(int number)
+/*void Numbers(int number)
 {
     while (number > 99999 || number <= 9999)
     {
@@ -27,4 +27,41 @@ void Numbers(int number)
 Console.Write("Введите пятизначное число, и мы проверим является ли оно палиндромом: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-Numbers(number);
+Numbers(number);*/
+
+// Задача 19 - 2. Напишите программу, которая принимает на вход ЛЮБОЕ число и проверяет, является ли оно палиндромом.
+int[] NumbersToArray(int number) // преобразуем в массив
+{
+    string str = number.ToString();
+    int[] num = new int[str.Length];
+    for (int i = 0; i < str.Length; i++) // Заполняем массив числами
+    {
+        num[i] = str[i];
+    }
+    return num;
+}
+void Check(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    int result = 0;
+    while (index < length)
+    {
+        if (array[index] != array[length - 1])
+        {
+            result = -1;
+            break;
+        }
+        result = 0;
+        index++;
+        length--;
+    }
+    if (result == 0) Console.Write("Число полиндром");
+    if (result == -1) Console.Write("Число НЕполиндром");
+}
+
+Console.Write("Введите пятизначное число, и мы проверим является ли оно палиндромом: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = NumbersToArray(number);
+Check(array);
